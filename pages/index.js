705d33f251657ec87a15a7e1d221d9ba6819db09
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2024-07-30 23:07:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home() { 
-    return <h1>Ansioso pelo cineminha contigo ;) </h1>
+function Home() {
+  return <h1>Ansioso pelo cineminha contigo ')</h1>;
+}
+function teste() {
+  return <h1>Testando componentes React</h1>;
 }
 
+function teste2() {
+  return <h1>Testando componentes React 2</h1>;
+}
 export default Home;
