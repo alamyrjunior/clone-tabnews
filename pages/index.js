8b0bbb6f627027d859e79eddf9adf2c50fb29922
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2024-07-30 23:12:58 +0000

    adiciona prettierignore

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
 function Home() {
   return <h1>Ansioso pelo cineminha contigo ')</h1>;
 }
-function teste() {
-  return <h1>Testando componentes React</h1>;
-}
-
-function teste2() {
-  return <h1>Testando componentes React 2</h1>;
-}
 export default Home;
