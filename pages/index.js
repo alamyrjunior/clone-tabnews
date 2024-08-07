adee60359f9734589ec7e1a2d1e2a537cb3d8d6a
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2024-08-07 02:05:34 +0000

    gif

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from "https://lottie.host/014a0424-c6b5-4ce0-aa06-fdb9165d2578/4vc0jiKdwg.json"; // ajuste o caminho para o seu arquivo JSON
+
 function Home() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <img src="https://lottie.host/014a0424-c6b5-4ce0-aa06-fdb9165d2578/4vc0jiKdwg.json" />
+    <div>
+      <h1>Este é um título H1</h1>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
   );
 }
+
 export default Home;
