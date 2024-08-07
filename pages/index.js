0a61e6d7ce4c044e5f7f2368dc885ea14e7bca49
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2024-08-07 02:01:39 +0000

    tentando por um gif outra vez

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function Home() {
-  return <h1> Educa Fórum</h1><img src="https://lottie.host/014a0424-c6b5-4ce0-aa06-fdb9165d2578/4vc0jiKdwg.json" />;
+  return (
+    <img src="https://lottie.host/014a0424-c6b5-4ce0-aa06-fdb9165d2578/4vc0jiKdwg.json" />
+  );
 }
 export default Home;
