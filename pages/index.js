f111b9718afd342e65f3bc25093c30eea2c9180f
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2024-08-07 01:57:44 +0000

    tentando por um gif

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-  return <h1>Ansioso pelo cineminha contigo ')</h1>;
+  return <h1> Educa Fórum</h1><img src="https://lottie.host/014a0424-c6b5-4ce0-aa06-fdb9165d2578/4vc0jiKdwg.json" />;
 }
 export default Home;
