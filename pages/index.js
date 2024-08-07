e4ef34698a9fa46d312eac58d87a9b1053abccc1
Alamyr Junior <63241800+alamyrjunior@users.noreply.github.com>
2024-08-07 02:11:01 +0000

    oo vida

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Lottie from "react-lottie";
-import animationData from "https://lottie.host/014a0424-c6b5-4ce0-aa06-fdb9165d2578/4vc0jiKdwg.json"; // ajuste o caminho para o seu arquivo JSON
+import animationData from "./pages/animation.json"; // ajuste o caminho para o seu arquivo JSON
 
 function Home() {
   const defaultOptions = {
